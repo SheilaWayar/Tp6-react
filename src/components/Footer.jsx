d583845b18370d3e28fd9efbec45c0aea4570311
Sheila sabrina lamas wayar <sheila.wayar.3@gmail.com>
2026-05-18 17:04:37 -0300

    Modificar Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   const año = new Date().getFullYear()
   return (
     <footer style={{padding: '1rem', background: '#282c34', color: 'white', textAlign: 'center', marginTop: '2rem'}}>
-      <p>© {año} Prácticas Profesionalizantes II</p>
+      <p>© {año} Sheila Sabrina Lamas Wayar </p>
     </footer>
   )
 }
